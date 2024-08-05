0097e2281b031bb9ce6faaa7f071d25589eb2383
Michael Gaziotis <Michael.Gaziotis@sea.live>
2024-08-05 14:44:30 +0100

    Final touches

--- source/interface/Madot.Interface.CLI/Responses/Common.cs
+++ source/interface/Madot.Interface.CLI/Responses/Common.cs
@@ -2,10 +2,7 @@
 
 namespace Madot.Interface.CLI.Responses;
 
-public record CreatedResponse([property: JsonPropertyName("id")]string Id);
-
-public record FailureResponse([property: JsonPropertyName("success")]string Success);
-
+public record CreatedResponse([property: JsonPropertyName("created")]string Created);
 public record ErrorResponse([property: JsonPropertyName("error")]string Error);
 public record NotFoundResponse([property: JsonPropertyName("notFound")]string NotFound);
 
